75daf65cc29b0eefcc5db38b6a3deb06e6c3f216
Sristi27 <sristi2705@gmail.com>
2020-09-06 00:43:18 +0530

    Updates

---
+++ precache-manifest.4c18c1e583ebbab2e80e574296bae54b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4abe610c88ca473cac2bb6563f7c7405",
+    "url": "/to-do-App/index.html"
+  },
+  {
+    "revision": "e3c44cae18aad9479140",
+    "url": "/to-do-App/static/css/main.d2143f76.chunk.css"
+  },
+  {
+    "revision": "b7803bd3c376ad528d73",
+    "url": "/to-do-App/static/js/2.94deef8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-App/static/js/2.94deef8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c44cae18aad9479140",
+    "url": "/to-do-App/static/js/main.48d7391d.chunk.js"
+  },
+  {
+    "revision": "a36a80d16565e1c9642d",
+    "url": "/to-do-App/static/js/runtime-main.0c702faa.js"
+  }
+]);
